aafb7bc3b49d3ff6587e3483622d016cc57ee110
Masuda-1246 <yaojiezengtian38@gmail.com>
2023-06-09 13:47:19 +0900

    update message

--- server/main.py
+++ server/main.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 def get_request():
     value = request.args.get('text', '')
     callback = request.args.get('callback', '')
-    dic = {'output' : [{'type' : 'text', 'value' : value }] }
+    message = f"「{value}」を受け取りました。"
+    dic = {'output' : [{'type' : 'text', 'value' : message }] }
     contents = callback + '(' + json.dumps(dic) + ')'
     return contents
 if __name__ == "__main__":
