b91a3f106100f657d718057f1015f9874eb29449
Masuda-1246 <yaojiezengtian38@gmail.com>
2023-06-12 11:10:51 +0900

    webpack

--- js/src/index.js
+++ js/src/index.js
@@ -1,1 +1,26 @@
-export { default as ChatUx }  from './chat-ux.js';
+import ChatUx from './chat-ux.js';
+
+const chatux = new ChatUx();
+const initParam =
+        {
+            renderMode: 'auto',
+            api: {
+                endpoint: 'http://0.0.0.0:80',
+                method: 'GET',
+                dataType: 'jsonp'
+            },
+            bot: {
+                botPhoto: 'https://res.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_170,w_170,f_auto,b_white,q_auto:eco,dpr_1/rkgkbhsyildogpvvwcxf',
+                humanPhoto: null,
+                widget: {
+                    sendLabel: '送信',
+                    placeHolder: '何か話しかけてみてください'
+                }
+            },
+            window: {
+                title: 'Chat AI Pro',
+                infoUrl: 'https://chat-aipro.80and.co/'
+            }
+        };
+chatux.init(initParam);
+chatux.start(true);
